8f40234a8f5bf9712a5cc6028c5dd40c37f7fb78
Gabriel de Oliveira <gabrieldeoliveira.gdo23@gmail.com>
2020-06-12 19:11:33 -0300

    (feat) setup

---
+++ SQL Server Connection (14-06)/src/main/App.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		String connectionUrl = "jdbc:sqlserver://localhost;databaseName=SampleDatabase;user=testeLogon;password=sample123";
+		String insertString = "INSERT INTO Pessoa (id, nome, idade) VALUES (?, ?, ?)";
+
+		try (
+			Connection connection = DriverManager.getConnection(connectionUrl);
+			PreparedStatement statement = connection.prepareStatement(insertString);
+		) {
+			Pessoa p1 = new Pessoa(1, "Maria", 50);
+
+			statement.setInt(1, p1.getId());
+			statement.setString(2, p1.getNome());
+			statement.setInt(3, p1.getIdade());
+
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
